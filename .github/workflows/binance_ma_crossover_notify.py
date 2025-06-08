33716f7941bfd6ed8a9d95392094a7122ff27f4a
Johntenn44 <johntenn44@gmail.com>
2025-06-08 23:28:57 +0100

    Update binance_ma_crossover_notify.py

--- .github/workflows/binance_ma_crossover_notify.py
+++ .github/workflows/binance_ma_crossover_notify.py
@@ -14,7 +14,7 @@ COINS = [
 ]
 
 EXCHANGE_ID = 'kraken'
-INTERVAL = '6h'      # <-- Changed from '1d' to '6h'
+INTERVAL = '1d'      # <-- Changed from '1d' to '6h'
 LOOKBACK = 210       # Number of candles to fetch (must be >= 200)
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
